36ff0ae74cc0cf8aba026574297af66b8b58dd12
hun2ya9 <hun2ya9@naver.com>
2017-05-29 23:27:44 +0900

    이거로 갈아끼우면 스테이지 클리어결과창 정상적으로 작동함

--- Mark/Assets/Scripts/StageResult.cs
+++ Mark/Assets/Scripts/StageResult.cs
@@ -7,14 +7,14 @@ public class StageResult : MonoBehaviour {
 
     Text D,T,U,S;
     string Difficulty;
-    string Time = Timer.time.ToString();
+    //string Time = Timer.time.ToString();
     string UseItem = Item.useItem.ToString();
 
     void Start()
     {
         D = GameObject.FindGameObjectWithTag("Difficulty").GetComponent<Text>();
         T = GameObject.FindGameObjectWithTag("Time").GetComponent<Text>();
-        U = GameObject.FindGameObjectWithTag("UseItem").GetComponent<Text>();
+        U = GameObject.FindGameObjectWithTag("UsedItem").GetComponent<Text>();
         S = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
         if (UIManager.level == 1) {
             Difficulty = "Easy";
@@ -25,10 +25,25 @@ public class StageResult : MonoBehaviour {
         else if (UIManager.level == 3) {
             Difficulty = "Hard";
         }
-        
-        D.text = Difficulty;
-        T.text = Time;
-        U.text = UseItem;
+
+        int tt = Mathf.FloorToInt(Timer.time);
+        float minute = Mathf.FloorToInt(tt / 60);
+        string second = tt.ToString();
+        string ResetSecond = (tt - 60 * Mathf.FloorToInt(tt / 60)).ToString();
+
+        if (tt < 60)
+        {
+            T.text = "플레이 시간 : 00분" + second + "초";
+        }
+        else if (tt >= 60)
+        {
+            T.text = "플레이 시간 : " + minute + "분" + ResetSecond + "초";
+        }
+
+
+        D.text = "난이도 :" + Difficulty;
+        //T.text = "플레이 시간 : " + Time;
+        U.text = "사용 아이템 수 : " + UseItem;
     }
 
     // Update is called once per frame
